7142a1fb310961ba00bddfd1d3b99351bd7e2c73
chriswakeley <chriswakeley@gmail.com>
2023-04-14 13:21:46 -0400

    Refactor flashing code

--- script.js
+++ script.js
@@ -1,27 +1,40 @@
-function handleClick(e){
-    this.classList.toggle("clicked")
-    let header = document.querySelector("h1");
-    header.classList.toggle("pre-clicked");
-    document.querySelector(".display").classList.toggle("pre-clicked");
-    //document.querySelector("#header").classList.toggle("pre-clicked");
-    
+//All elements that should flash when "=" is clicked
+const flashableElements = document.querySelectorAll("h1, .display");
+
+
+// elements is a nodelist of elements to flash
+function flashElements(elements){
+    elements.forEach((element) => element.classList.toggle("pre-clicked"));
 }
-function endTransition(e){
-    if(e.propertyName !== "color") return;
-    if(this.classList.contains('pre-clicked')){
+
+function handleClick(e) {
+    this.classList.toggle("clicked");
+    flashElements(flashableElements);
+}
+
+function endTransition(e) {
+    //ignore all but color to limit one call per transition
+    if (e.propertyName !== "color") return;
+
+    //handle flashable elements in pre-clicked state
+    if (this.classList.contains('pre-clicked')) {
         this.classList.toggle('pre-clicked');
         this.classList.toggle('clicked');
         return;
     }
-    if(this.classList.contains('clicked')){
+
+    //handle buttons and flashable elements when in 'clicked' state
+    if (this.classList.contains('clicked')) {
         this.classList.toggle('clicked');
     }
-
 }
+
+
 document.querySelectorAll("button").forEach(thisbutton => {
     thisbutton.addEventListener('click', handleClick);
+    thisbutton.addEventListener('transitionend', endTransition);
 });
 
-document.querySelectorAll("h1, .display, button").forEach(element => {
+flashableElements.forEach(element => {
     element.addEventListener('transitionend', endTransition);
 });
