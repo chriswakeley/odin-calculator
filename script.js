77cf295738015ad9e061c9d0667e4589ce4cba99
chriswakeley <chriswakeley@gmail.com>
2023-04-13 23:50:52 -0400

    Modify flashing behavior

--- script.js
+++ script.js
@@ -2,35 +2,26 @@ function handleClick(e){
     this.classList.toggle("clicked")
     let header = document.querySelector("h1");
     header.classList.toggle("pre-clicked");
+    document.querySelector(".display").classList.toggle("pre-clicked");
     //document.querySelector("#header").classList.toggle("pre-clicked");
     
 }
 function endTransition(e){
-    /*if(this.classList.contains('pre-clicked') && !(this.classList.contains('clicked'))){
+    if(e.propertyName !== "color") return;
+    if(this.classList.contains('pre-clicked')){
+        this.classList.toggle('pre-clicked');
         this.classList.toggle('clicked');
         return;
     }
-    if(!(this.classList.contains('clicked'))) return;
-    this.classList.toggle("clicked");
-    if(this.classList.contains("pre-clicked")){
-        this.classList.toggle("pre-clicked");
-    }*/
-    if(e.propertyName !== "color") return;
-    if(e.elapsedTime <= 0.05 && this.classList.contains('pre-clicked') ){
-        this.classList.toggle("clicked");
-        return;
+    if(this.classList.contains('clicked')){
+        this.classList.toggle('clicked');
     }
-    if(e.elapsedTime <= 0.1 && this.classList.contains('clicked')){
-        this.classList.toggle("clicked");
-        if(this.classList.contains('pre-clicked')){
-            this.classList.toggle("pre-clicked");
-        }
-    }
+
 }
 document.querySelectorAll("button").forEach(thisbutton => {
     thisbutton.addEventListener('click', handleClick);
 });
 
-document.querySelectorAll("*").forEach(element => {
+document.querySelectorAll("h1, .display, button").forEach(element => {
     element.addEventListener('transitionend', endTransition);
 });
